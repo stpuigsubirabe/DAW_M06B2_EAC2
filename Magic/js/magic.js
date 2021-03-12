5be4f2ccee539be7f3266f78d92f24f073d3b826
spuigsubira@gmail.com <stpuigsubirabe>
2021-03-12 16:42:50 +0100

    Càrrega de fitxers

---
+++ Magic/js/magic.js
@@ -0,0 +1,13 @@
+function transformar_text(text_simbols){
+	//Convertim els salts de línia a format HTML
+	text_transformat = text_simbols.replace(/\r?\n/g, "<br />");
+	//Cerquem el text a transformar
+	var trobar = /\{[A-Z0-9/]*\}/g;
+	//Canviem el text per les imatges corresponents
+	var text_transformat = text_transformat.replace(trobar, function(x){
+		//Afagem el text de dintre de les {} treient les / interiors (si hi ha alguna)
+		var lletres = x.substring(1,x.length-1).replace('/','');
+		return "<img src='https://c2.scryfall.com/file/scryfall-symbols/card-symbols/"+lletres+".svg' class='mana-icon'>"
+	});
+	return text_transformat;
+}
